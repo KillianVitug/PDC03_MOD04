4ad3fede98c90e858a5dec1697a49b047da08f0d
Vitug Killian <killianjarelvitug@yahoo.com>
2023-04-28 10:49:45 +0800

    first commit

---
+++ PDC03_MODULE4/PDC03_MODULE4/activity02.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODULE4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class activity02 : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+        public activity02()
+        {
+            InitializeComponent();
+            EmployeeView.ItemsSource = employees;
+
+            employees.Add(new Employee { DisplayName = "Killian", Position = "President" });
+            employees.Add(new Employee { DisplayName = "Vitug", Position = "President" });
+            employees.Add(new Employee { DisplayName = "Jarel", Position = "President" });
+            employees.Add(new Employee { DisplayName = "Cabiling", Position = "President" });
+        }
+
+        private void Add_item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            {
+
+            }
+        }
+
+        private void Edit_item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            {
+
+            }
+        }
+
+        private void Delete_item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            {
+
+            }
+        }
+    }
+}
